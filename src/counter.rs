4ec51663687aca2efab0498097fedc3c4228ce80
Y0hy0h <Y0hy0h@users.noreply.github.com>
2019-03-24 22:15:02 +0100

    Make simple counter app

---
+++ src/counter.rs
@@ -0,0 +1,38 @@
+use std::fs;
+use std::sync::atomic::AtomicUsize;
+use std::sync::atomic::Ordering;
+
+const FILE_PATH: &str = "count.db";
+
+pub struct Counter(AtomicUsize);
+
+impl Counter {
+    pub fn new() -> Counter {
+        let count = fs::read_to_string(FILE_PATH)
+            .map_err(|_| "File could not be read.")
+            .and_then(|count_string| {
+                count_string
+                    .parse()
+                    .map_err(|_| "File contained illegal count.")
+            })
+            .unwrap_or_else(|error| {
+                eprintln!("{}", error);
+                0
+            });
+        Counter(AtomicUsize::new(count))
+    }
+
+    pub fn get_count(&self) -> usize {
+        self.0.load(Ordering::Relaxed)
+    }
+
+    pub fn increment(&self) {
+        let new_count = fetch_plus(&self.0, 1, Ordering::Relaxed);
+        fs::write(FILE_PATH, format!("{}", new_count)).expect("File was not writable.");
+    }
+}
+
+/// Like `AtomicUsize::fetch_add`, but returns the updated value.
+fn fetch_plus(atom: &AtomicUsize, val: usize, order: Ordering) -> usize {
+    atom.fetch_add(val, order) + val
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+mod counter;
+
+#[macro_use]
+extern crate rocket;
+use counter::Counter;
+use rocket::State;
+
+#[get("/")]
+fn index(counter: State<Counter>) -> String {
+    let count = counter.get_count();
+    counter.increment();
+    format!("Visitor number: {}", count)
+}
+
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite()
+        .mount("/", routes![index])
+        .manage(Counter::new())
+        .launch();
 }
