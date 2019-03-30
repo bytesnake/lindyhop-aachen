f3efe9a3bd6141c7cac7edadc060efa0ebe75fa2
Y0hy0h <Y0hy0h@users.noreply.github.com>
2019-03-30 19:29:34 +0100

    Mount Elm admin

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,8 @@ mod events;
 extern crate rocket;
 use rocket::State;
 
+use rocket_contrib::serve::StaticFiles;
+
 use events::{Event, Location, Occurrence};
 
 use chrono::prelude::*;
@@ -33,29 +35,38 @@ fn render_event(event: &Event) -> Markup {
 struct Events<'a>(Vec<Event<'a>>);
 
 const CHICO: Location = Location {
-                name: "Chico Mendès",
-                address: "Aachen",
-            };
+    name: "Chico Mendès",
+    address: "Aachen",
+};
 
 fn main() {
     rocket::ignite()
-    .manage(Events(vec![Event {
-        name: "Social Dance",
-        teaser: "Einfach tanzen.",
-        description: "Lindy Hop tanzen in einer Bar.",
-        occurrences: vec![Occurrence {
-            start: Local.ymd(2019, 4, 1).and_hms(20, 30, 00),
-            duration: chrono::Duration::minutes(90),
-            location: &CHICO,
-        }],
-    }, Event {
-        name: "Anfängerkurs",
-        teaser: "Hereinschnuppern.",
-        description: "Ein Einführung für diejenigen, die noch nie Lindy Hop getanzt haben.",
-        occurrences: vec![Occurrence {
-            start: Local.ymd(2019, 4, 1).and_hms(19, 45, 00),
-            duration: chrono::Duration::minutes(45),
-            location: &CHICO,
-        }]
-    }])).mount("/", routes![index]).launch();
+        .manage(Events(vec![
+            Event {
+                name: "Social Dance",
+                teaser: "Einfach tanzen.",
+                description: "Lindy Hop tanzen in einer Bar.",
+                occurrences: vec![Occurrence {
+                    start: Local.ymd(2019, 4, 1).and_hms(20, 30, 00),
+                    duration: chrono::Duration::minutes(90),
+                    location: &CHICO,
+                }],
+            },
+            Event {
+                name: "Anfängerkurs",
+                teaser: "Hereinschnuppern.",
+                description: "Ein Einführung für diejenigen, die noch nie Lindy Hop getanzt haben.",
+                occurrences: vec![Occurrence {
+                    start: Local.ymd(2019, 4, 1).and_hms(19, 45, 00),
+                    duration: chrono::Duration::minutes(45),
+                    location: &CHICO,
+                }],
+            },
+        ]))
+        .mount("/admin", {
+            let path = concat!(env!("CARGO_MANIFEST_DIR"), "/admin/dist");
+            StaticFiles::from(path)
+        })
+        .mount("/", routes![index])
+        .launch();
 }
