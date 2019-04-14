8c9f72fe4dffbc0add970d9a58977487c85028a5
Y0hy0h <Y0hy0h@users.noreply.github.com>
2019-04-14 18:09:26 +0200

    Fix watch script

--- watch-admin.sh
+++ watch-admin.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cargo watch -s "(cd admin/Elm && yarn build) && cargo run" -i "admin/dist/*" -i "admin/Elm/elm-stuff/**/*" -i "admin/Elm/node_modules/**/*"
+cargo watch -s "cd admin/Elm && yarn build && cd ../.. && cargo run" -i "admin/dist/*" -i "admin/Elm/elm-stuff/**/*" -i "admin/Elm/node_modules/**/*"
